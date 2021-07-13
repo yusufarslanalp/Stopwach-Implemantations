49847351d55608ba070ca95d6dee5ec217c18111
Yusuf Abdullah Arslanalp <yusuf.arslanalp@piworks.net>
2021-07-13 13:42:57 +0300

    feat: stopwatch working. no class. no button.

--- stopwatch.js
+++ stopwatch.js
@@ -1,14 +1,27 @@
-document.getElementById("time").innerHTML = " changed!";
+document.getElementById("time").innerHTML = "0";
 
 
-var i = 0;
+
+var total_sec = 0;
+var display_second = 0;
+var minute = 0;
+var hour = 0;
 
 	(async function () {
         while ( true ) {
-            await new Promise(r => setTimeout(r, 1000));
-            i += 1;
-            console.log( i );
-            document.getElementById("time").innerHTML = i;
+            await new Promise(r => setTimeout(r, 1));
+            total_sec += 1;
+            display_second = total_sec % 60;
+            console.log( total_sec );
+            if( total_sec % 60 == 0 )
+            {
+                minute += 1;
+                minute = minute % 60;
+            }
+            if( total_sec % 3600 == 0 ) hour += 1; 
+            document.getElementsByClassName("second")[0].innerHTML = display_second;
+            document.getElementsByClassName("minute")[0].innerHTML = minute;
+            document.getElementsByClassName("hour")[0].innerHTML = hour;
         }
     })();
 
