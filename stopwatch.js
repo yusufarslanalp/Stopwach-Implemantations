ee07561d9ae7f9963c138d48bdac7be34c6edc4b
Yusuf Abdullah Arslanalp <yusuf.arslanalp@piworks.net>
2021-07-24 11:17:32 +0300

    refactor: name of stopwach.html file converted to index.html

--- stopwatch.js
+++ stopwatch.js
@@ -1,19 +1,29 @@
 function Stopwatch( scp ) {
     this.scp = scp;
-    this.divRef;
 
     this.totalSec = 0;
     this.displaySecond = 0;
     this.minute = 0;
     this.hour = 0;
-    this.startTime = true;
+    this.startTime = false;
+
+    this.show = true;
+    this.startBtnShow = true;
+    this.stopBtnShow = false;
 
     this.incrementSecond = 	async function () {
-        while ( this.startTime ) {
+        while ( true ) {
             await new Promise(r => setTimeout(r, 1000 ));
+            //to remove if statement this.scp.$apply(); shold be at the beginning
+            //of the loop. But this.scp.$apply(); gives error at the beginning of the loop.
+            //If you don't know how to fix this, don't remove if statement.
+            if( this.startTime === false )
+            {
+                break;
+            }
             this.totalSec += 1;
             this.updateTime();
-            this.scp.$apply(); //if you put this line begining of the loop it gives an error.
+            this.scp.$apply(); //if you put this line to the beginning of the loop it gives an error.
         }
     };
 
@@ -21,19 +31,19 @@ function Stopwatch( scp ) {
         this.displaySecond = this.totalSec % 60 ;
         this.minute = parseInt( ( this.totalSec % 3600) / 60, 10 ); 
         this.hour = parseInt( this.totalSec / 3600, 10 );
-
-        //$( this.divRef ).find( ".stwSecond" ).text( (this.displaySecond).pad(2) );
     }
 
     this.start = function() {
         this.startTime = true;
+        this.startBtnShow = false;
+        this.stopBtnShow = true;
         this.incrementSecond();
-
     }
 
     this.stop = function() {
         this.startTime = false;
-     
+        this.startBtnShow = true;
+        this.stopBtnShow = false;
     }
 
     this.reset = function(){
@@ -44,15 +54,8 @@ function Stopwatch( scp ) {
 
     this.delete = function(){
         this.stop();
+        this.show = false;
     }
 
-    //this.start();
     this.incrementSecond();
-}
-
-
-Number.prototype.pad = function(size) {
-    var s = String(this);
-    while (s.length < (size || 2)) {s = "0" + s;}
-    return s;
 }
